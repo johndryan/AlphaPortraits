9487649c5a0f12d4442cf218dcb7aedac0fdfcc9
John Ryan <johndryan@gmail.com>
2013-03-27 12:38:45 -0700

    Starting from scratch!

--- CoherentLinePlanning/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ofApp.h"
-#include "ofAppGlutWindow.h"
-
-int main() {
-	ofAppGlutWindow window;
-	window.setGlutDisplayString("rgba double samples>=8 depth");
-	ofSetupOpenGL(&window, 800, 800, OF_FULLSCREEN);
-	ofRunApp(new ofApp());
-}
